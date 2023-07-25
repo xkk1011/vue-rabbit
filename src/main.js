dfb82012a904475d78167932c34f335e2258b6f6
xkk1011 <2909176429@qq.com>
2023-07-25 13:22:46 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// 测试接口函数
+import { getCategory } from "@/apis/testAPI";
+getCategory().then((res) => {
+  console.log(res);
+});
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
