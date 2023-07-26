a8bda22d22dd198784d73dc6f15f863f8e834367
xkk1011 <2909176429@qq.com>
2023-07-26 19:44:43 +0800

    一级分类-按照业务逻辑拆分函数

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,36 +1,15 @@
 <script setup>
-import { getCategoryAPI } from "@/apis/category";
-import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
-import { getBannerAPI } from "@/apis/home";
+
 import GoodsItem from "../Home/components/GoodsItem.vue";
-import { onBeforeRouteUpdate } from "vue-router";
 
-// 获取数据
-const categoryData = ref({});
-// 获取路由参数
-const route = useRoute();
-const getCategory = async (id = route.params.id) => {
-  const res = await getCategoryAPI(id);
-  categoryData.value = res.result;
-};
-onMounted(() => getCategory());
+import { useBanner } from "./composables/useBanner";
+import { useCategory } from "./composables/useCategory";
 
-// 目标：路由参数变化的时候 可以把分类数据接口重新发送
-onBeforeRouteUpdate((to) => {
-  // 存在问题：使用最新的路由参数请求最新的分类数据
-  getCategory(to.params.id);
-});
+// 获取分类数据
+const { categoryData } = useCategory();
 
 // 获取banner
-const bannerList = ref([]);
-const getBanner = async () => {
-  const res = await getBannerAPI({
-    distributionSite: "2",
-  });
-  bannerList.value = res.result;
-};
-onMounted(() => getBanner());
+const { bannerList } = useBanner();
 </script>
 
 <template>
