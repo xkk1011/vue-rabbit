d809a191f75a11e59ac34dbff59d2efd82b9807f
xkk1011 <2909176429@qq.com>
2023-07-26 16:17:34 +0800

    home-面板组件封装

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,58 @@
+<script setup>
+// 组件参数包括props和插槽
+// 非复杂的模板抽象成props，复杂的模板抽象成插槽
+// 定义props
+defineProps({
+  // 主标题
+  title: {
+    type: String,
+  },
+  // 副标题
+  subTitle: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+          {{ title }}<small>{{ subTitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域 -->
+      <!-- 抽象成插槽传入 -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
