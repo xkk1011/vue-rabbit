00a9cb095ec5fa3f696f798fa5fc6d614e8088a4
xkk1011 <2909176429@qq.com>
2023-07-27 11:29:57 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,11 +1,20 @@
 import httpInstance from "@/utils/http";
 
 // 获取二级分类列表
-export function getCategoryAPI(id) {
+export const getCategoryAPI = (id) => {
   return httpInstance({
     url: "/category",
     params: {
       id,
     },
   });
-}
+};
+
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url: "/category/sub/filter",
+    params: {
+      id,
+    },
+  });
+};
