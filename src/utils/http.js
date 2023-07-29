a75d5c49cd860eff00df56e09a529c254fe56bc0
xkk1011 <2909176429@qq.com>
2023-07-29 19:24:49 +0800

    登录基础业务实现

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
 // axios基础的封装
 import axios from "axios";
+import "element-plus/theme-chalk/el-message.css";
+import { ElMessage } from "element-plus";
 
 const httpInstance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -20,6 +22,8 @@ httpInstance.interceptors.request.use(
 httpInstance.interceptors.response.use(
   (res) => res.data,
   (e) => {
+    // 统一错误提示
+    ElMessage({ type: "warning", message: e.response.data.message });
     return Promise.reject(e);
   }
 );
