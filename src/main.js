a0a0294f36a5740c7b1aa4d79a08302a0f6b6162
xkk1011 <2909176429@qq.com>
2023-07-30 11:41:57 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from "./App.vue";
 import router from "./router";
@@ -21,8 +22,10 @@ import { lazyPlugin } from "@/directives/";
 import { componentPlugin } from "@/components";
 
 const app = createApp(App);
-
-app.use(createPinia());
+const pinia = createPinia();
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 app.use(router);
 app.use(lazyPlugin);
 app.use(componentPlugin);
